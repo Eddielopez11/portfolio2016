d7b0509e077e0aef655574acf3040da47c9c1419
Eduardo Lopez <eddielopez2101@gmail.com>
2016-12-07 16:47:40 -0600

    all set and ready to go

--- build.js
+++ build.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
 import Home from './lib/Home'
+import Nav from './lib/Nav'
 
 render((
   <Router history={ hashHistory }>
-    <Route path="/" component={ Nav }>
+    <Route path="/nav" component={ Nav }>
       <Route path="/" component={ Home }/>
     </Route>
   </Router>
-), document.getElementsById('app'))
+), document.getElementById('app'))
