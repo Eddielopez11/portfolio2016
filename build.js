d2b83e2bdc479c5d79a727c675f1ab2d4447236d
Eduardo Lopez <eddielopez2101@gmail.com>
2016-12-07 19:37:47 -0600

    tried out parallax scrolling and got it all set up

--- build.js
+++ build.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
-import Home from './lib/Home'
+import About from './lib/About'
 import Nav from './lib/Nav'
 
 render((
   <Router history={ hashHistory }>
     <Route path="/nav" component={ Nav }>
-      <Route path="/" component={ Home }/>
+      <Route path="/" component={ About }/>
     </Route>
   </Router>
 ), document.getElementById('app'))
