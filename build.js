ec54d566a19b341dbd7eceda32b689cea83afbb3
Eduardo Lopez <eddielopez2101@gmail.com>
2016-12-12 17:38:02 -0600

    changed files and setup around, got rid of react router for now as it was not nessecary, still working on links to work correct with parallax scrolling issue

--- build.js
+++ build.js
@@ -1,13 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'
 import About from './lib/About'
-import Nav from './lib/Nav'
 
 render((
-  <Router history={ hashHistory }>
-    <Route path="/nav" component={ Nav }>
-      <Route path="/" component={ About }/>
-    </Route>
-  </Router>
+    <About/>
 ), document.getElementById('app'))
