bd8f182dc045ddd3eed878ef5e87b6c833a45a88
Eduardo Lopez <eddielopez2101@gmail.com>
2016-12-13 01:07:23 -0600

    brought in two fonts from google, restructured nav and home files now that everything is aligning

--- build.js
+++ build.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
 import About from './lib/About'
+import Nav from './lib/Nav'
 
 render((
+  <div className="mainPageWrapper">
+    <Nav/>
     <About/>
+  </div>
 ), document.getElementById('app'))
